0b58a4209aa4fcafb7ddc0fe9445977ca1b8fb57
shaikhry <shaikhriyaz783@gmail.com>
2023-03-17 12:03:39 +0530

    Updates

---
+++ precache-manifest.642093c5a516e178256582a7d82edf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3b429fcb2356f0c73d1",
+    "url": "/Project-assignment/static/js/main.a19dad22.chunk.js"
+  },
+  {
+    "revision": "a2a586b0f13b8fb7178b",
+    "url": "/Project-assignment/static/js/runtime~main.1b94b08a.js"
+  },
+  {
+    "revision": "0e2bae77c86f8a1fe785",
+    "url": "/Project-assignment/static/js/2.3bbe1a65.chunk.js"
+  },
+  {
+    "revision": "84c91a5e42742e8f24a03190670b4616",
+    "url": "/Project-assignment/static/media/Logo2.84c91a5e.png"
+  },
+  {
+    "revision": "50d088567f0586b55916197770cc581c",
+    "url": "/Project-assignment/static/media/A.T-inks-logo.50d08856.png"
+  },
+  {
+    "revision": "24097c7e15fdd4c3d09e398aadf042c2",
+    "url": "/Project-assignment/static/media/profilePhoto.24097c7e.jpg"
+  },
+  {
+    "revision": "0cdbd47ff0463fdfb3570725c720b122",
+    "url": "/Project-assignment/index.html"
+  }
+];
